d145ca2318bd8426bc4137acccdbf13fa41720c3
raulnegrau <48685351+raulnegrau@users.noreply.github.com>
2020-10-16 16:19:32 +0300

    Added an idea for document class!

---
+++ src/sistem_birocratic/entity/Document.java
@@ -0,0 +1,82 @@
+package sistem_birocratic.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Document {
+    protected String documentName;
+    protected List<Document> intermediateDocumentList;
+    private boolean documentState; // 1 => document procurat, 0 => document neprocurat
+    private boolean documentType; // 1 => exista subdocumente, 0 => nu exista subdocumente
+
+    public Document(String documentName, List<Document> intermediateDocumentList){
+        this.documentName = documentName;
+        if(intermediateDocumentList == null)
+            this. intermediateDocumentList = new ArrayList<Document>();
+        else this.intermediateDocumentList = intermediateDocumentList;
+        this.documentState = false;
+        this.documentType = false;
+    }
+
+    public Document(String documentName, List<Document> intermediateDocumentList, boolean documentType){
+        this.documentName = documentName;
+        if(intermediateDocumentList == null)
+            this. intermediateDocumentList = new ArrayList<Document>();
+        else this.intermediateDocumentList = intermediateDocumentList;
+        this.documentState = false;
+        this.documentType = documentType;
+    }
+
+    //getters
+    public String getDocumentName(){
+        return documentName;
+    }
+    public List<Document> getIntermediateDocumentList(){
+        return intermediateDocumentList;
+    }
+    public boolean getDocumentState(){
+        return this.documentState;
+    }
+    public boolean getDocumentType(){
+        return documentType;
+    }
+
+    //setters
+    public void setDocumentName(String documentName){
+        this.documentName = documentName;
+    }
+    public void setIntermediateDocumentList(List<Document> intermediateDocumentList){
+        this.intermediateDocumentList = intermediateDocumentList;
+    }
+    public void setDocumentState(boolean documentState){
+        this.documentState = documentState;
+    }
+    public void setDocumentType(boolean documentType){
+        this.documentType = documentType;
+    }
+
+    public boolean checkDocument() {
+        // true when all anexes are true
+        //boolean otherDocumentsState = true;
+        if(intermediateDocumentList.isEmpty()){
+            return documentState;
+        }
+        //System.out.println("Am trecut de primul if!");
+        if(documentState){
+            for(Document document : intermediateDocumentList){
+                if(!document.checkDocument()) {
+                    //System.out.println("False in for");
+                    return false;
+                }
+            }
+            return true;
+        }
+        //System.out.println("Ultimul return false!");
+        return false;
+    }
+
+    public String toString(){
+        return documentName;
+    }
+
+}
